8077e9642ea4ab7d0c91682c2d82072d0a42f193
EurusFox <157649617+eurusfox-24@users.noreply.github.com>
2025-11-22 20:17:37 +0200

    Update portscanner.py

--- RedTeam/portscanner.py
+++ RedTeam/portscanner.py
@@ -1,27 +1,26 @@
-import socket   
-          
-          def scan(target, ports):
-              print('\n' + ' Starting Scan for ' + str(target))
-              for port in range(1, ports):
-                  scan_port(target, port)
-          
-          def scan_port(ipaddress, port):
-              try:
-                  sock = socket.socket()
-                  sock.connect((ipaddress, port))
-                  print('[+] Port open ' +str(port))
-                  port.close()
-              except:
-                  pass
-          
-          
-          targets = input('[*] Enter targets to scan(split them by ,): ')
-          ports = int(input('[*] Enter how many ports you want to scan: '))
-          
-          if ',' in targets:
-              print('[*] Scanning multiple targets')
-              for ip_addr in targets.split(','):
-                  scan(ip_addr.strip(''), ports)
-          
-          else: 
-              scan(targets, ports)
+import socket
+
+def scan(target, ports):
+    print('\n' + 'Starting Scan for ' + str(target))
+    for port in range(1, ports + 1):
+        scan_port(target, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print(f"[+] Port open {port}")
+        sock.close()
+    except:
+        pass
+
+targets = input('[*] Enter targets to scan (split them by ,): ')
+ports = int(input('[*] Enter how many ports you want to scan: '))
+
+if ',' in targets:
+    print('[*] Scanning multiple targets')
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(), ports)
+else:
+    scan(targets, ports)
